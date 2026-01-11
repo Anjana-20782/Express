0e1c394f3fb1f1670dc8eb6e87d0f847ee07a0d8
Anjana-20782 <anjanasivadasan0305@gmail.com>
2026-01-11 12:07:08 +0530

    aysync wrapper added

--- Express-basics/Fourth/app.js
+++ Express-basics/Fourth/app.js
@@ -8,4 +8,16 @@ app.use(express.json());
 // routes
 app.use("/api/products", productRoutes);
 
-export default app;   
+/* -------- GLOBAL ERROR HANDLER (REQUIRED) -------- */
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.message = err.message || "Internal Server Error";
+
+  res.status(err.statusCode).json({
+    status: "error",
+    message: err.message,
+  });
+});
+/* ------------------------------------------------ */
+
+export default app;
--- Express-basics/Fourth/controllers/product.controller.js
+++ Express-basics/Fourth/controllers/product.controller.js
@@ -1,9 +1,15 @@
 import Product from "../models/Product.model.js";
+import catchAsync from "../utils/catchAsync.js";
 
-export const getProducts = async (req, res) => {
-  const products = await Product.find();
-  res.json(products);
-};
+// export const getProducts = async (req, res) => {
+//   const products = await Product.find();             //36
+//   res.json(products);
+// };
+
+export const getProducts = catchAsync(async (req, res) => {
+  throw new Error("Test error from catchAsync");            //37
+});
+
 
 export const createProduct = async (req, res) => {
   const product = await Product.create(req.body);
